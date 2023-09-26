e19df67efd552a3fa020e13d170675026a9c527f
peak2 <36886305+peak2@users.noreply.github.com>
2023-09-26 14:55:46 +0100

    react-typescript-tailwind-vite-antd setup

---
+++ ui_design_task2/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+ import {Button} from "antd"
+function App() {
+
+  return (
+  <div className='bg-green-300 h-screen'>
+ <Button className='bg-cyan-500 text-green-200'> welcome Home</Button>
+  </div>
+  )
+}
+
+export default App
